6dd0cbbcb1c8a7750d2e69448e72915ea90917e8
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-20 14:38:19 +0100

    Move autopublish to subscribe/publication

--- imports/api/posts.js
+++ imports/api/posts.js
@@ -1,3 +1,11 @@
+import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
 
+
 export const Posts = new Mongo.Collection('posts')
+
+if (Meteor.isServer) {
+  Meteor.publish({
+    'posts.find': () => Posts.find()
+  })
+}
