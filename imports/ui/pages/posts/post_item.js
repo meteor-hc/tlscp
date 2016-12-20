a8dc0159f0ef55531e4956283dd7a65b239ba4c9
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-20 12:10:44 +0100

    Add posts logic

--- imports/ui/pages/posts/post_item.js
+++ imports/ui/pages/posts/post_item.js
@@ -1,1 +1,10 @@
 import './post_item.html'
+
+Template.postItem.helpers({
+  domain: () => {
+    const post = Template.instance().data
+    const a = document.createElement('a')
+    a.href = post.url
+    return a.hostname
+  }
+})
